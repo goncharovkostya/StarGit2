33b0221dc4e009497609b63d3f365f61d3ce1d3b
slegka0 <44324112+slegka0@users.noreply.github.com>
2019-03-16 16:10:20 +0300

    COMPLETED PROJECT

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mainButton;
+    TextView mainTextView;
+    int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.button);
+        a = 0;
+        mainButton = findViewById(R.id.main_button);
+        mainTextView = findViewById(R.id.answer);
+        mainButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText("" + ++a);
     }
 }
